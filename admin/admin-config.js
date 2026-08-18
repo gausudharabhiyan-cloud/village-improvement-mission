abdb90d17e3cdf25f57255887a97326a23de75a6
gausudharabhiyan-cloud <gausudharabhiyan@gmail.com>
2026-08-18 14:32:33 +0530

    Update admin-config.js

--- admin/admin-config.js
+++ admin/admin-config.js
@@ -1,5 +1,5 @@
 // अपना existing Google Apps Script Web App URL यहां डालें
-const ADMIN_API_URL="https://script.google.com/macros/s/AKfycbwRHW9-fTGGNorChrZgCEJVHvkm70StA2dhSvbb5XNi1TwQAq9TT6uhNozBwfaFOeiP9w/exec";
+const ADMIN_API_URL="https://script.google.com/macros/s/AKfycbwBBIMpl5n2Dmq91D2fvc1-skWBjgQOCduWptQbjjvqr9k-_krgHOgCpX06Vy1G8qsn8A/exec";
 
 // Admin login credentials
 // इन्हें अपनी पसंद से बदलें
