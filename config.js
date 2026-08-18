2f754ef4e3a0ee8403822092c3656d03e100a0b7
gausudharabhiyan-cloud <gausudharabhiyan@gmail.com>
2026-08-18 10:42:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-/*
-  GOOGLE APPS SCRIPT WEB APP URL यहां डालें.
-  Example:
-  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/XXXXXXXX/exec";
-*/
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwRHW9-fTGGNorChrZgCEJVHvkm70StA2dhSvbb5XNi1TwQAq9TT6uhNozBwfaFOeiP9w/exec";
+// IMPORTANT: यहां अपनी मौजूदा Google Apps Script Web App URL रखें.
+// उदाहरण: const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/XXXX/exec";
+const GOOGLE_SCRIPT_URL = "Phttps://script.google.com/macros/s/AKfycbwRHW9-fTGGNorChrZgCEJVHvkm70StA2dhSvbb5XNi1TwQAq9TT6uhNozBwfaFOeiP9w/exec";
